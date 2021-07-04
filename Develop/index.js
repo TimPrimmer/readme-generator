018e361c39429a27c2155fc705714ba2a4f0fc71
Tim Primmer <timprimmer@gmail.com>
2021-07-04 16:40:41 -0500

    added basic question inputs

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,93 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+
+
+/*
+Title of project
+Description
+Table of Contents
+Installations
+Usage
+License
+Contributing
+Tests
+Questions
+*/
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your projects title?",
+    validate(value) {
+      if (!value) {
+        console.log("please enter in a title!")
+        return false;
+      }
+      else { return true; }
+    }
+  },
+  {
+    type: "input",
+    name: "desc",
+    message: "Describe your project:",
+    validate(value) {
+      if (!value) {
+        console.log("please enter in a description!")
+        return false;
+      }
+      else { return true; }
+    }
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "How do you install this project?",
+    validate(value) {
+      if (!value) {
+        console.log("please provide installation instructions!")
+        return false;
+      }
+      else { return true; }
+    }
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use this project once installed?",
+    validate(value) {
+      if (!value) {
+        console.log("please provide usage information!")
+        return false;
+      }
+      else { return true; }
+    }
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "If you would like others to contribute, please enter in some guidelines:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "How would you test this project?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => { }
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+  console.log("oooo");
+  inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, '  '));
+  });
+}
 
 // Function call to initialize app
 init();
